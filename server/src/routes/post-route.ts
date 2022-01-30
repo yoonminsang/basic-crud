62ef71d9e0a8dd92aed684c338c2ef660b13fa10
yoonminsang <apdlyooapdl@naver.com>
2022-01-30 11:07:54 +0900

    feat: read post list validation

--- server/src/routes/post-route.ts
+++ server/src/routes/post-route.ts
@@ -1,5 +1,6 @@
 import PostController from '@/controllers/post-controller';
 import createPostValidation from '@/validation/post/create-post-validation';
+import readPostListValidation from '@/validation/post/read-post-list-validation';
 import { Router } from 'express';
 
 const postRouter = Router();
@@ -8,7 +9,7 @@ const postController = new PostController();
 
 postRouter.post('/', createPostValidation, postController.createPost);
 // postRouter.get('/:id', , postController.readPost);
-postRouter.get('/', postController.readPostList);
+postRouter.get('/', readPostListValidation, postController.readPostList);
 // postRouter.put('/:id',  postController.updatePost);
 // postRouter.delete('/:id', postController.deletePost);
 
---
+++ server/src/validation/post/read-post-list-validation.ts
@@ -0,0 +1,21 @@
+import { COMMON_ERROR } from '@/constants/error';
+import CustomError from '@/error/custom-error';
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+const readPostListValidation = (req: Request, res: Response, next: NextFunction): void => {
+  const schema = Joi.object({
+    pageId: Joi.number().integer().positive().required().empty(''),
+    postNumber: Joi.number().integer().positive().required().empty(''),
+  });
+  const validationResult = schema.validate(req.query);
+  const { error } = validationResult;
+  console.log(error);
+  if (error) {
+    throw new CustomError({ ...COMMON_ERROR.invalidQuery });
+  }
+
+  next();
+};
+
+export default readPostListValidation;
