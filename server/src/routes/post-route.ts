6b53c7993e9b438407596c3f5d32b5ba5912b722
yoonminsang <apdlyooapdl@naver.com>
2022-01-30 11:23:35 +0900

    feat: read post validation

--- server/src/routes/post-route.ts
+++ server/src/routes/post-route.ts
@@ -1,6 +1,7 @@
 import PostController from '@/controllers/post-controller';
 import createPostValidation from '@/validation/post/create-post-validation';
 import readPostListValidation from '@/validation/post/read-post-list-validation';
+import readPostValidation from '@/validation/post/read-post-validation';
 import { Router } from 'express';
 
 const postRouter = Router();
@@ -8,7 +9,7 @@ const postRouter = Router();
 const postController = new PostController();
 
 postRouter.post('/', createPostValidation, postController.createPost);
-postRouter.get('/:id', postController.readPost);
+postRouter.get('/:id', readPostValidation, postController.readPost);
 postRouter.get('/', readPostListValidation, postController.readPostList);
 // postRouter.put('/:id',  postController.updatePost);
 // postRouter.delete('/:id', postController.deletePost);
