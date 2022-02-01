8350e8fe09ce57a66401015b0a67a4f863ad410d
yoonminsang <apdlyooapdl@naver.com>
2022-02-02 05:07:17 +0900

    feat: 버튼 컴포넌트 생성

---
+++ client/src/components/common/button/index.tsx
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import jsx from '@/core/jsx';
+import Component from '@/core/component';
+import './style.css';
+
+interface IProps {
+  href?: string;
+  type?: string;
+  text: string;
+}
+
+class Button extends Component {
+  constructor(target: HTMLElement, props: IProps) {
+    super(target, props);
+  }
+
+  public markup(): string {
+    const { href, type, text } = this.props;
+    return href ? (
+      <a class="button" href={href}>
+        {text}
+      </a>
+    ) : (
+      <button class="button" type={type || 'button'}>
+        {text}
+      </button>
+    );
+  }
+}
+
+export default Button;
