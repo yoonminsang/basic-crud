fd58c0132e20f0292daa196b27ddc3fe6aea8a81
yoonminsang <apdlyooapdl@naver.com>
2022-02-02 05:07:25 +0900

    feat: 헤더 컴포넌트 생성

---
+++ client/src/components/common/header/index.tsx
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import jsx from '@/core/jsx';
+import Component from '@/core/component';
+import './style.css';
+
+class Header extends Component {
+  public markup(): string {
+    return <h1>BASIC CRUD</h1>;
+  }
+}
+
+export default Header;
