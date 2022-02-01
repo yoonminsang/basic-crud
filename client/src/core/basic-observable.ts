e23c15d396f91c582150c5215b752ab646b806d8
yoonminsang <apdlyooapdl@naver.com>
2022-02-02 05:06:45 +0900

    feat: 로컬 스토어 생성(옵저버)

--- client/src/core/basic-observable.ts
+++ client/src/core/basic-observable.ts
@@ -9,10 +9,23 @@ abstract class Observable {
     this.state = {};
   }
 
-  public setState(nextState: TState, cb?: Function) {
-    this.state = { ...this.state, ...nextState };
-    this.notify();
-    cb?.();
+  public setState(changeState: TState, cb?: Function) {
+    if (!this.checkNeedUpdate(changeState)) return;
+    requestAnimationFrame(() => {
+      this.state = { ...this.state, ...changeState };
+      this.notify();
+      cb?.();
+    });
+  }
+
+  private checkNeedUpdate(changeState: TState) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in changeState) {
+      if (!Object.is(changeState[key], this.state[key])) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public subscribe(observer: Function) {
