18ac68c00ba67ead16b3f3a9d96a627a94bce095
yoonminsang <apdlyooapdl@naver.com>
2022-02-03 10:54:29 +0900

    feat: 검색 컴포넌트 구현

--- client/src/components/post-list/index.tsx
+++ client/src/components/post-list/index.tsx
@@ -8,6 +8,7 @@ import './style.css';
 import { useHistory } from '@/core/routerHooks';
 import { IRouterState } from '@/core/types';
 import { parseTime } from '@/utils/parser';
+import Search from './search';
 
 interface IState {
   postList: IPost[];
@@ -27,37 +28,49 @@ class PostList extends Component {
   public markup(): string {
     const { postList } = this.state;
     return (
-      <ul class="post-list">
-        <li class="post-title">
-          <div class="title">제목</div>
-          <div class="user">유저</div>
-          <div class="date">작성일</div>
-        </li>
+      <div>
+        {postList.length ? (
+          <ul class="post-list">
+            <li class="post-title" key="0">
+              <div class="title">제목</div>
+              <div class="user">유저</div>
+              <div class="date">작성일</div>
+            </li>
 
-        {postList.map(({ title, user, date, id }) => (
-          <li key={id} class="post-item">
-            <a class="title" href={`/post/${id}`}>
-              {title}
-            </a>
-            <div class="user">{user}</div>
-            <div class="date">{parseTime(date)}</div>
-          </li>
-        ))}
-      </ul>
+            {postList.map(({ title, user, date, id }) => (
+              <li key={id} class="post-item">
+                <a class="title" href={`/post/${id}`}>
+                  {title}
+                </a>
+                <a class="user" href={`/post/search?searchType=user&searchContent=${user}`}>
+                  {user}
+                </a>
+                <div class="date">{parseTime(date)}</div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          // TODO: 스타일
+          <div>글 목록이 존재하지 않습니다</div>
+        )}
+        <div class="content-search" component />
+      </div>
     );
+  }
+
+  public appendComponent(target: HTMLElement): void {
+    const $search = target.querySelector('.content-search') as HTMLElement;
+    new Search($search);
   }
 
   public async componentDidMount() {
     const { pathname, query } = this.history;
     const { searchType, searchContent } = query;
     const pageId = query.pageId || 1;
-    if (pathname === '/search') {
-      // TODO: else 처리?
-      if (searchType && searchContent) {
-        postStore.subscribe(() =>
-          this.setState({ postList: postStore.getCashSearchPostList(searchType, searchContent, pageId) }),
-        );
-      }
+    if (pathname === '/post/search' && searchType && searchContent) {
+      postStore.subscribe(() =>
+        this.setState({ postList: postStore.getCashSearchPostList(searchType, searchContent, pageId) }),
+      );
       postStore.getSearchPostList(searchType, searchContent, pageId);
     } else {
       postStore.subscribe(() => this.setState({ postList: postStore.getCashPostList(pageId) }));
---
+++ client/src/components/post-list/search/index.tsx
@@ -0,0 +1,89 @@
+/** @jsx jsx */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import jsx from '@/core/jsx';
+import Component from '@/core/component';
+import './style.css';
+import Input from '@/components/common/input';
+import Button from '@/components/common/button';
+import { TSearchType } from '@/types/IPost';
+import { useHistory } from '@/core/routerHooks';
+import { IRouterState } from '@/core/types';
+
+const getSearchType = (searchType: TSearchType) => {
+  const obj = { title: '제목', content: '내용', user: '닉네임' };
+  return obj[searchType];
+};
+
+interface IState {
+  input: string;
+  searchType: TSearchType;
+  dropdown: boolean;
+}
+
+class Search extends Component {
+  state: IState;
+  history: IRouterState;
+
+  constructor(target: HTMLElement) {
+    super(target);
+    this.state = { input: '', searchType: 'title', dropdown: false };
+    this.onSearchHandler = this.onSearchHandler.bind(this);
+    this.history = useHistory();
+  }
+
+  // TODO: 드롭다운
+  public markup(): string {
+    const searchTypes: TSearchType[] = ['title', 'content', 'user'];
+    return (
+      <form class="form-search">
+        <div class="input-container" component></div>
+        <div class="dropdown-container">
+          <div class="btn-dropdown-container" component />
+          <div class={`dropdown ${!this.state.dropdown ? 'blind' : ''}`}>
+            {searchTypes.map((searchType) => (
+              <button type="button" class="js-search-type" data-search-type={searchType}>
+                {getSearchType(searchType)}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div class="btn-container" component></div>
+      </form>
+    );
+  }
+
+  public appendComponent(target: HTMLElement): void {
+    const $input = target.querySelector('.input-container') as HTMLElement;
+    const $dropdownButton = target.querySelector('.btn-dropdown-container') as HTMLElement;
+    const $button = target.querySelector('.btn-container') as HTMLElement;
+    new Input($input, { type: 'text', value: this.state.input, placeholder: '검색할 내용을 입력하세요' });
+    new Button($dropdownButton, {
+      type: 'button',
+      text: getSearchType(this.state.searchType),
+      class: 'white js-dropdown',
+    });
+    new Button($button, { type: 'submit', text: '검색' });
+  }
+
+  public setDelegation(): void {
+    this.addDelegation('input', '.input-container', (e: Event) => {
+      this.setState({ input: (e.target as HTMLInputElement).value });
+    });
+    this.addDelegation('click', '.js-dropdown', () => {
+      this.setState({ dropdown: !this.state.dropdown });
+    });
+    this.addDelegation('click', '.js-search-type', (e: Event) => {
+      const searchType = (e.target as HTMLElement).dataset.searchType as string;
+      this.setState({ dropdown: !this.state.dropdown, searchType });
+    });
+    this.addDelegation('submit', '.form-search', this.onSearchHandler);
+  }
+
+  private onSearchHandler(e: Event) {
+    e.preventDefault();
+    const { searchType, input } = this.state;
+    this.history.push(`/post/search?searchType=${searchType}&searchContent=${input}`);
+  }
+}
+
+export default Search;
