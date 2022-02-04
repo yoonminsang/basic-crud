d60c14f718f215356202d6eda13850537f82fdc3
yoonminsang <apdlyooapdl@naver.com>
2022-02-05 02:37:30 +0900

    chore: 주석제거

--- server/src/error/error-generator.ts
+++
@@ -1,7 +0,0 @@
-// import CustomError from './custom-error';
-
-// const errorGenerator = (message: string, customMessage?: string): CustomError => {
-//   return new CustomError(message, customMessage);
-// };
-
-// export default errorGenerator;
