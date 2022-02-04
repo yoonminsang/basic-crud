5370757bef346b75f1d0b9052a941ba27981ef2d
yoonminsang <apdlyooapdl@naver.com>
2022-02-04 09:02:16 +0900

    wip: 드롭다운 컴포넌트 재새ㅏ용

--- client/src/components/post-list/search/index.tsx
+++ client/src/components/post-list/search/index.tsx
@@ -8,7 +8,9 @@ import Button from '@/components/common/button';
 import { TSearchType } from '@/types/IPost';
 import { useHistory } from '@/core/routerHooks';
 import { IRouterState } from '@/core/types';
+import Dropdown from '@/components/common/dropdown';
 
+const searchTypes: TSearchType[] = ['title', 'content', 'user'];
 const getSearchType = (searchType: TSearchType) => {
   const obj = { title: '제목', content: '내용', user: '닉네임' };
   return obj[searchType];
@@ -17,7 +19,6 @@ const getSearchType = (searchType: TSearchType) => {
 interface IState {
   searchContent: string;
   searchType: TSearchType;
-  dropdown: boolean;
 }
 
 class Search extends Component {
@@ -26,26 +27,16 @@ class Search extends Component {
 
   constructor(target: HTMLElement) {
     super(target);
-    this.state = { searchContent: '', searchType: 'title', dropdown: false };
+    this.state = { searchContent: '', searchType: 'title' };
     this.onSearchHandler = this.onSearchHandler.bind(this);
     this.history = useHistory();
   }
 
   public markup(): string {
-    const searchTypes: TSearchType[] = ['title', 'content', 'user'];
     return (
       <form class="form-search">
         <div class="input-container" component></div>
-        <div class="dropdown-container">
-          <div class="btn-dropdown-container" component />
-          <div class={`dropdown ${!this.state.dropdown ? 'blind' : ''}`}>
-            {searchTypes.map((searchType) => (
-              <button type="button" class="js-search-type" data-search-type={searchType}>
-                {getSearchType(searchType)}
-              </button>
-            ))}
-          </div>
-        </div>
+        <div class="dropdown-container" component />
         <div class="btn-container" component></div>
       </form>
     );
@@ -53,27 +44,21 @@ class Search extends Component {
 
   public appendComponent(target: HTMLElement): void {
     const $input = target.querySelector('.input-container') as HTMLElement;
-    const $dropdownButton = target.querySelector('.btn-dropdown-container') as HTMLElement;
     const $button = target.querySelector('.btn-container') as HTMLElement;
+    const $dropdown = target.querySelector('.dropdown-container') as HTMLElement;
     new Input($input, { type: 'text', value: this.state.searchContent, placeholder: '검색할 내용을 입력하세요' });
-    new Button($dropdownButton, {
-      type: 'button',
-      text: getSearchType(this.state.searchType),
-      class: 'white js-dropdown',
-    });
     new Button($button, { type: 'submit', text: '검색' });
+    new Dropdown($dropdown, {
+      dropdowns: searchTypes,
+      changeText: getSearchType,
+      selectedDropdown: this.state.searchType,
+      eventHandler: (searchType: TSearchType) => this.setState({ searchType }),
+    });
   }
 
   public setDelegation(): void {
     this.addDelegation('input', '.input-container', (e: Event) => {
       this.setState({ searchContent: (e.target as HTMLInputElement).value });
-    });
-    this.addDelegation('click', '.js-dropdown', () => {
-      this.setState({ dropdown: !this.state.dropdown });
-    });
-    this.addDelegation('click', '.js-search-type', (e: Event) => {
-      const searchType = (e.target as HTMLElement).dataset.searchType as string;
-      this.setState({ dropdown: !this.state.dropdown, searchType });
     });
     this.addDelegation('submit', '.form-search', this.onSearchHandler);
   }
