009f6d70b36cde8b2d4cce4e04246c2f388f4692
yoonminsang <apdlyooapdl@naver.com>
2022-02-04 14:13:43 +0900

    feat: false, undefined 인 경우에 children 제거

--- client/src/core/jsx.ts
+++ client/src/core/jsx.ts
@@ -4,16 +4,20 @@ type TState = Record<string, any>;
 export interface IJsx {
   type: string;
   props: TState;
-  children: IJsx[];
+  children: any[] | IJsx[];
 }
 
-function jsx(type: string, props: TState, ...children: IJsx[]): IJsx {
+function jsx(type: string, props: TState, ...children: any[] | IJsx[]): IJsx {
+  const nextChildren: IJsx[] = [];
+  children.forEach((child) => {
+    if (child !== false && child !== undefined) nextChildren.push(child);
+  });
   if (props) {
     Object.keys(props).forEach((key) => {
       if (props[key] === false || props[key] === undefined) delete props[key];
     });
   }
-  return { type, props, children: children.flat() };
+  return { type, props, children: nextChildren.flat() };
 }
 
 export default jsx;
