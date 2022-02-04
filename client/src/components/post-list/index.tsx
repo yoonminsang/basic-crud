90ad09dd41cb7f24cbd4b50d17f7dfe486ad4089
yoonminsang <apdlyooapdl@naver.com>
2022-02-04 14:07:48 +0900

    feat: 페이지네이션 컴포넌트 구현

--- client/src/components/post-list/index.tsx
+++ client/src/components/post-list/index.tsx
@@ -2,7 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import jsx from '@/core/jsx';
 import Component from '@/core/component';
-import { IPost } from '@/types/IPost';
+import { IPost, TSearchType } from '@/types/IPost';
 import postStore from '../../store/post-store';
 import './style.css';
 import { useHistory } from '@/core/routerHooks';
@@ -10,11 +10,13 @@ import { IRouterState } from '@/core/types';
 import { parseTime } from '@/utils/parser';
 import Search from './search';
 import PostListHeader from './post-list-header';
+import Pagination from '../common/pagination';
 
 interface IState {
   postList?: IPost[];
   postNumber: number;
   isDescending: number;
+  pageCount: 1;
 }
 
 class PostList extends Component {
@@ -23,7 +25,7 @@ class PostList extends Component {
 
   constructor(target: HTMLElement) {
     super(target);
-    this.state = { postList: undefined, postNumber: 30, isDescending: 1 };
+    this.state = { postList: undefined, postNumber: 30, isDescending: 1, pageCount: 1 };
     this.history = useHistory();
   }
 
@@ -32,28 +34,32 @@ class PostList extends Component {
     if (!postList) return '';
     return (
       <div class="post-list-wrapper">
+        {false}
         <div class="post-list-header-container" component />
         <div class="content-search" component />
         {postList && postList.length ? (
-          <ul class="post-list">
-            <li class="post-title" key="0">
-              <div class="title">제목</div>
-              <div class="user">닉네임</div>
-              <div class="date">작성일</div>
-            </li>
-
-            {postList.map(({ title, user, date, id }) => (
-              <li key={id} class="post-item">
-                <a class="title" href={`/${id}`}>
-                  {title}
-                </a>
-                <a class="user" href={`/search?searchType=user&searchContent=${user}`}>
-                  {user}
-                </a>
-                <div class="date">{parseTime(date)}</div>
+          <frag>
+            <ul class="post-list">
+              <li class="post-title" key="0">
+                <div class="title">제목</div>
+                <div class="user">닉네임</div>
+                <div class="date">작성일</div>
               </li>
-            ))}
-          </ul>
+
+              {postList.map(({ title, user, date, id }) => (
+                <li key={id} class="post-item">
+                  <a class="title" href={`/${id}`}>
+                    {title}
+                  </a>
+                  <a class="user" href={`/search?searchType=user&searchContent=${user}`}>
+                    {user}
+                  </a>
+                  <div class="date">{parseTime(date)}</div>
+                </li>
+              ))}
+            </ul>
+            <div class="pagination-container" component />
+          </frag>
         ) : (
           // TODO: 스타일
           <div>글 목록이 존재하지 않습니다</div>
@@ -63,12 +69,14 @@ class PostList extends Component {
   }
 
   public appendComponent(target: HTMLElement): void {
-    const { postList, postNumber, isDescending } = this.state;
+    const { postList, postNumber, isDescending, pageCount } = this.state;
     if (!postList) return;
     const $header = target.querySelector('.post-list-header-container') as HTMLElement;
     const $search = target.querySelector('.content-search') as HTMLElement;
+    const $pagination = target.querySelector('.pagination-container') as HTMLElement;
     new PostListHeader($header, { postNumber, isDescending });
     new Search($search);
+    new Pagination($pagination, { pageCount, showCount: 5 });
   }
 
   public async componentDidMount() {
@@ -77,23 +85,34 @@ class PostList extends Component {
       const { searchType, searchContent } = query;
       const pageId = query.pageId || 1;
       if (pathname === '/search' && searchType && searchContent) {
-        await postStore.getSearchPostList(searchType, searchContent, pageId);
-        postStore.subscribe(() =>
-          this.setState({ postList: postStore.getCashSearchPostList(searchType, searchContent, pageId) }),
-        );
+        this.getSearchPostList(searchType, searchContent, pageId);
       } else {
-        await postStore.getPostList(pageId);
-        postStore.subscribe(() =>
-          this.setState({
-            postList: postStore.getCashPostList(pageId),
-            isDescending: postStore.state.isDescending,
-            postNumber: postStore.state.postNumber,
-          }),
-        );
+        this.getPostList(pageId);
       }
     } catch (err) {
       console.error(err);
     }
+  }
+
+  private async getSearchPostList(searchType: TSearchType, searchContent: string, pageId: number) {
+    await postStore.getSearchPostList(searchType, searchContent, pageId);
+    postStore.subscribe(() => {
+      const { postList, pageCount } = postStore.getCashSearchPostList(searchType, searchContent, pageId);
+      this.setState({ postList, pageCount });
+    });
+  }
+
+  private async getPostList(pageId: number) {
+    await postStore.getPostList(pageId);
+    postStore.subscribe(() => {
+      const { postList, pageCount } = postStore.getCashPostList(pageId);
+      this.setState({
+        postList,
+        pageCount,
+        isDescending: postStore.state.isDescending,
+        postNumber: postStore.state.postNumber,
+      });
+    });
   }
 
   public async componentDidUpdate(state: IState, nextState: IState) {
