8e56f51e52d98f81f74877eecb745156a99889ad
yoonminsang <apdlyooapdl@naver.com>
2022-02-05 11:01:43 +0900

    wip: validation 상수 분리

--- client/src/utils/validation/post-validation.ts
+++ client/src/utils/validation/post-validation.ts
@@ -1,25 +1,18 @@
+import { POST_ERROR } from '@/constants/validaiton';
 import { POST_ENTITY } from '@/constants/entity/post';
 
-const ERROR = {
-  exceedMaxLengthTitle: `제목은 ${POST_ENTITY.titleMaxLength}자를 넘길 수 없습니다`,
-  fillTitle: `제목을 입력해주세요`,
-  exceedMaxLengthUser: `닉네임은 ${POST_ENTITY.userMaxLength}자를 넘길 수 없습니다`,
-  underMinLengthUser: `닉네임은 ${POST_ENTITY.userMinLength}자 이상 입력해야 합니다`,
-  fillUser: `닉네임을 입력해주세요`,
-};
-
 const createValidation = (title: string, user: string) => {
-  if (!title) return ERROR.fillTitle;
-  if (title.length > POST_ENTITY.titleMaxLength) return ERROR.exceedMaxLengthTitle;
-  if (!user) return ERROR.fillUser;
-  if (user.length > POST_ENTITY.userMaxLength) return ERROR.exceedMaxLengthUser;
-  if (user.length < POST_ENTITY.userMinLength) return ERROR.underMinLengthUser;
+  if (!title) return POST_ERROR.fillTitle;
+  if (title.length > POST_ENTITY.titleMaxLength) return POST_ERROR.exceedMaxLengthTitle;
+  if (!user) return POST_ERROR.fillUser;
+  if (user.length > POST_ENTITY.userMaxLength) return POST_ERROR.exceedMaxLengthUser;
+  if (user.length < POST_ENTITY.userMinLength) return POST_ERROR.underMinLengthUser;
   return true;
 };
 
 const updateValidation = (title: string) => {
-  if (!title) return ERROR.fillTitle;
-  if (title.length > POST_ENTITY.titleMaxLength) return ERROR.exceedMaxLengthTitle;
+  if (!title) return POST_ERROR.fillTitle;
+  if (title.length > POST_ENTITY.titleMaxLength) return POST_ERROR.exceedMaxLengthTitle;
   return true;
 };
 
